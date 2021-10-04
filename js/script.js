9883b3f5271f18bb786a6f18a76ad11eae8c8219
Kai Packwood <kai.packwood@igd.com>
2021-10-04 10:26:29 +0100

    Global variables created

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Page elements
+const guessedList = document.querySelector(".guessed-letters");
+const button = document.querySelector(".guess");
+const input = document.querySelector(".letter");
+const currentWord = document.querySelector(".word-in-progress");
+const remainingText = document.querySelector(".remaining");
+const remainingGuesses = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgain = document.querySelector(".play-again");
+
+// Word being guessed
+const word = "magnoloa";
